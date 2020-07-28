7b935f1761f0ec7198d02ae2c3c9f23cbdd345ac
Joseph Valenciano <JosephV27@users.noreply.github.com>
2020-07-27 18:11:37 -0600

    optimizando ac_atan2

--- proto/ac_atan2.m
+++ proto/ac_atan2.m
@@ -17,72 +17,78 @@
 
 function [theta, octant] = ac_atan2(x, y)
 
-numerador = int32(x * y); #numerator operation
+#constants
+PI = 102944; #pi Q(15)
+PI_MEDIOS = 51472; #pi/2 Q(15)
+PI_CUARTOS = 25736; #pi/4 Q(15)
+TRES_PI_CUARTOS = 77208; #(3*pi)/4 Q(15)
 
-op1 = int32(bitshift ((y * y), -2)); #Q >> 2
-op2 = int32(bitshift ((y * y), -5)); #Q >> 5
 
-op_denoI = int32((x * x) + op1 + op2); #op in denominator with I^2
-op_denoQ = int32((y * y) + op1 + op2); #op in denominator with Q^2 
+numerator = int32(x * y); #numerator operation
 
-denominadorI = int32(bitshift (op_denoI, -15));
-denominadorQ = int32(bitshift (op_denoQ, -15));
+y_square = y * y;
+x_square = x * x; 
 
-resultadoI = int32(numerador / denominadorI); #result with denominator with I^2
-resultadoQ = int32(numerador / denominadorQ); #result with denominator with Q^2 
+op1 = int32(bitshift ((y_square), -2)); #Q >> 2
+op2 = int32(bitshift ((y_square), -5)); #Q >> 5
 
-pi_entero = int32(round(pi * 32768)); #pi Q(15)
-pi_medios = int32(round((pi/2) * 32768)); #pi/2 Q(15)
-pi_cuartos = int32(round((pi/4) * 32768)); #pi/4 Q(15)
-tres_pi_cuartos = int32(round(((3*pi)/4) * 32768)); #(3*pi)/4 Q(15)
+op_denoI = int32((x_square) + op1 + op2); #op in denominator with I^2
+op_denoQ = int32((y_square) + op1 + op2); #op in denominator with Q^2 
 
-if x == 8192 && y == 0 #0 degrees
+denominatorI = int32(bitshift (op_denoI, -15));
+denominatorQ = int32(bitshift (op_denoQ, -15));
+
+abs_x = abs(x); #Absolute value of x
+abs_y = abs(y); #Absolute value of y
+
+
+if x>0 && y == 0 #0 degrees
   theta = 0;
   octant = 0;
-elseif  abs(x) > abs(y) && x > 0 && y > 0  #first octant
-  theta = resultadoI;
+elseif  abs_x > abs_y && x > 0 && y > 0  #first octant
+  theta = numerator / denominatorI;;
   octant = 1;
-elseif  abs(x) == abs(y) && x > 0 && y > 0 #45 degrees
-  theta = pi_cuartos;
+elseif  abs_x == abs_y && x > 0 && y > 0 #45 degrees
+  theta = PI_CUARTOS;
   octant = 0;
-elseif  abs(x) < abs(y) && x > 0 && y > 0 #second octant
-  theta = pi_medios - resultadoQ;
+elseif  abs_x < abs_y && x > 0 && y > 0 #second octant
+  theta = PI_MEDIOS - (numerator / denominatorQ);
   octant = 2; 
 elseif  x==0 && y>0 #90 degrees
-  theta = pi_medios;
+  theta = PI_MEDIOS;
   octant = 0;
-elseif  abs(x) < abs(y) && x<0 && y>0 #third octant  
-  theta = pi_medios - resultadoQ;
+elseif  abs_x < abs_y && x<0 && y>0 #third octant  
+  theta = PI_MEDIOS - (numerator / denominatorQ);
   octant = 3;
-elseif  abs(x) == abs(y) && x<0 && y>0 #135 degrees
-  theta = tres_pi_cuartos;
+elseif  abs_x == abs_y && x<0 && y>0 #135 degrees
+  theta = TRES_PI_CUARTOS;
   octant = 0;
-elseif  abs(x) > abs(y) && x<0 && y>0 #fourth octant 
-  theta = pi_entero + resultadoI;
+elseif  abs_x > abs_y && x<0 && y>0 #fourth octant 
+  theta = PI + (numerator / denominatorI);
   octant = 4;
-elseif  abs(x) > abs(y) && x<0 && y<0 #fifth octant
-  theta = resultadoI - pi_entero;
+elseif  abs_x > abs_y && x<0 && y<0 #fifth octant
+  theta = (numerator / denominatorI) - PI;
   octant = 5;
-elseif  abs(x) == abs(y) && x<0 && y<0 #-135 degrees
-  theta = -tres_pi_cuartos;
+elseif  abs_x == abs_y && x<0 && y<0 #-135 degrees
+  theta = -TRES_PI_CUARTOS;
   octant = 0;
-elseif  abs(x) < abs(y) && x<0 && y<0 #sixth octant
-  theta = -pi_medios - resultadoQ;
+elseif  abs_x < abs_y && x<0 && y<0 #sixth octant
+  theta = -PI_MEDIOS - (numerator / denominatorQ);
   octant = 6;
 elseif  x==0 && y<0 #-90 degrees
-  theta = -pi_medios;
+  theta = -PI_MEDIOS;
   octant = 0;
-elseif  abs(x) < abs(y) && x>0 && y<0 #seventh octant
-  theta = -pi_medios - resultadoQ;
+elseif  abs_x < abs_y && x>0 && y<0 #seventh octant
+  theta = -PI_MEDIOS - (numerator / denominatorQ);
   octant = 7;
-elseif  abs(x) == abs(y) && x>0 && y<0 #-45 degrees
-  theta = -pi_cuartos;
+elseif  abs_x == abs_y && x>0 && y<0 #-45 degrees
+  theta = -PI_CUARTOS;
   octant = 0;
-elseif  abs(x) > abs(y) && x>0 && y<0 #eighth octant
-  theta = resultadoI;
+elseif  abs_x > abs_y && x>0 && y<0 #eighth octant
+  theta = numerator / denominatorI;
   octant = 8;  
 else
-  theta = pi_entero; #180 degrees
+  theta = PI; #180 degrees
   octant = 0;
 endif
 
