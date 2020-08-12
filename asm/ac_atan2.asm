76effd0f0e7710b0da10e87bb3b9c9db0481790f
Joseph Valenciano <JosephV27@users.noreply.github.com>
2020-08-11 22:57:41 -0600

    agregando mejoras ac_atan2

--- asm/ac_atan2.asm
+++ asm/ac_atan2.asm
@@ -1,4 +1,5 @@
 include c:\Irvine\Irvine32.inc
+include c:\Irvine\Macros.inc
 includelib c:\Irvine\Irvine32.lib
 includelib c:\Irvine\Kernel32.lib
 includelib c:\Irvine\user32.lib
@@ -11,8 +12,8 @@ PI_MEDIOS DWORD 51472
 PI_CUARTOS DWORD 25736
 TRES_PI_CUARTOS DWORD 77208
 
-;x DWORD -714
-;y DWORD 8161
+arreglo DWORD 73 DUP(?)
+count DWORD 4
 
 numerator DWORD ?
 y_square DWORD ?
@@ -38,12 +39,8 @@ octant DWORD ?
 
 .code 
 ac_atan2 PROC, x:DWORD, y:DWORD
-xor eax, eax
-xor ebx, ebx
-xor ecx, ecx 
-xor edx, edx
-
-
+push esi
+xor esi, esi
 ;-------------numerator
 mov eax, x
 mov ebx, y
@@ -111,47 +108,66 @@ L3:
 	mov abs_y, eax
 L4: 
 	mov abs_y, eax
-;-----------------move vars to registers
-mov eax, abs_x
-mov ebx, x
-mov edx, y
 ;-----------------0 degrees
-cmp ebx, 0 ; x > 0
+mov eax, abs_x
+cmp x, 0 ; x > 0
 JL P1
-cmp edx, 0 ; y == 0
+cmp y, 0 ; y == 0
 JNE P1
 mov eax, 0
-mov theta, eax
+;mov theta, eax
+mov esi, offset arreglo
+mov [esi], eax
+;mov eax, [esi]
+;call writeint
 ;-----------------First octant
 P1:
+	mov eax, abs_x
 	cmp eax, abs_y ; abs_x > abx_y
 	JLE P2
-	cmp ebx, 0 ; x > 0
+	cmp x, 0 ; x > 0
 	JL P2
-	cmp edx, 0 ; y > 0
-	JL P2
+	cmp y, 0 ; y > 0
+	JLE P2
 	mov eax, numerator
 	mov ebx, denominatorI
 	xor edx, edx
 	idiv ebx
-	mov theta, eax	
+	;mov theta, eax
+	mov esi, offset arreglo
+	mov edx, count
+	mov [esi+edx], eax
+	;mov eax, [esi+edx]
+	add edx, 4
+	mov count, edx
+	;call writeint
 ;-----------------45 degrees
 P2:
+	mov eax, abs_x
 	cmp eax, abs_y ; abx_x == abx_y
 	JNE P3
-	cmp ebx, 0 ; x > 0
+	cmp x, 0 ; x > 0
 	JL P3
-	cmp edx, 0 ; y > 0
+	cmp y, 0 ; y > 0
 	JL P3
 	mov eax, PI_CUARTOS
-	mov theta, eax
+	;mov theta, eax
+
+	mov esi, offset arreglo
+	mov edx, count
+	mov [esi+edx], eax
+	;mov eax, [esi+edx]
+	add edx, 4
+	mov count, edx
+	;call writeint
 ;-----------------Second octant
 P3:
+	mov eax, abs_x
 	cmp eax, abs_y ; abs_x < abs_y
 	JGE P4
-	cmp ebx, 0 ; x > 0
+	cmp x, 0 ; x > 0
 	JLE P4
-	cmp edx, 0 ; y > 0
+	cmp y, 0 ; y > 0
 	JLE P4
 	mov eax, numerator
 	mov ebx, denominatorQ
@@ -160,21 +176,39 @@ P3:
 	mov edx, PI_MEDIOS 
 	sub edx, eax
 	mov theta, edx
+	mov eax, edx
+
+	mov esi, offset arreglo
+	mov edx, count
+	mov [esi+edx], eax
+	;mov eax, [esi+edx]
+	add edx, 4
+	mov count, edx
+	;call writeint
 ;-----------------90 Degrees
 P4:
-	cmp ebx, 0
+	mov eax, abs_x
+	cmp x, 0 ;x==0
 	JNE P5
-	cmp edx, 0 ; y > 0
+	cmp y, 0 ; y > 0
 	JL P5
 	mov eax, PI_MEDIOS
-	mov theta, eax
+	;mov theta, eax
+	mov esi, offset arreglo
+	mov edx, count
+	mov [esi+edx], eax
+	;mov eax, [esi+edx]
+	add edx, 4
+	mov count, edx
+	;call writeint
 ;-----------------Third octant
 P5:
+	mov eax, abs_x
 	cmp eax, abs_y ; abs_x < abs_y
 	JGE P6
-	cmp ebx, 0 ; x < 0
+	cmp x, 0 ; x < 0
 	JGE P6
-	cmp edx, 0 ; y > 0
+	cmp y, 0 ; y > 0
 	JLE P6
 	mov eax, numerator
 	neg eax
@@ -185,64 +219,113 @@ P5:
 	mov edx, PI_MEDIOS 
 	sub edx, eax
 	mov theta, edx
+	mov eax, edx
+
+	mov esi, offset arreglo
+	mov edx, count
+	mov [esi+edx], eax
+	;mov eax, [esi+edx]
+	add edx, 4
+	mov count, edx
+	;call writeint
 ;-----------------135 Degrees
 P6:
-	cmp eax, abs_y ; abx_x == abx_y
+	mov eax, abs_x
+	cmp eax, abs_y ; abs_x == abx_y
 	JNE P7
-	cmp ebx, 0 ; x < 0
+	cmp x, 0 ; x < 0
 	JGE P7
-	cmp edx, 0 ; y > 0
+	cmp y, 0 ; y > 0
 	JLE P7
 	mov eax, TRES_PI_CUARTOS
-	mov theta, eax
+	;mov theta, eax
+
+	mov esi, offset arreglo
+	mov edx, count
+	mov [esi+edx], eax
+	;mov eax, [esi+edx]
+	add edx, 4
+	mov count, edx
+	;call writeint
 ;-----------------Fourth octant
 P7:
+	mov eax, abs_x
 	cmp eax, abs_y ; abs_x > abx_y
 	JLE P8
-	cmp ebx, 0 ; x < 0
+	cmp x, 0 ; x < 0
 	JGE P8
-	cmp edx, 0 ; y > 0
+	cmp y, 0 ; y > 0
 	JLE P8
 	mov eax, numerator
+	neg eax
 	mov ebx, denominatorI
 	xor edx, edx
 	idiv ebx
+	neg eax
 	mov edx, PI
 	add edx, eax
 	mov theta, edx
+	mov eax, edx
+
+	mov esi, offset arreglo
+	mov edx, count
+	mov [esi+edx], eax
+	;mov eax, [esi+edx]
+	add edx, 4
+	mov count, edx
+	;call writeint
 ;-----------------Fifth octant
 P8:
+	mov eax, abs_x
 	cmp eax, abs_y ; abs_x > abx_y
 	JLE P9
-	cmp ebx, 0 ; x < 0
+	cmp x, 0 ; x < 0
 	JGE P9
-	cmp edx, 0 ; y < 0
+	cmp y, 0 ; y < 0
 	JGE P9
 	mov eax, numerator
 	mov ebx, denominatorI
 	xor edx, edx
 	idiv ebx
 	mov edx, PI
-	sub eax, edx ; revisar
-	mov theta, eax
+	sub eax, edx
+	;mov theta, eax
+
+	mov esi, offset arreglo
+	mov edx, count
+	mov [esi+edx], eax
+	;mov eax, [esi+edx]
+	add edx, 4
+	mov count, edx
+	;call writeint
 ;----------------- -135 Degrees
 P9:
-	cmp eax, abs_y ; abx_x == abx_y
+	mov eax, abs_x
+	cmp eax, abs_y ; abs_x == abx_y
 	JNE P10
-	cmp ebx, 0 ; x < 0
+	cmp x, 0 ; x < 0
 	JGE P10
-	cmp edx, 0 ; y < 0
+	cmp y, 0 ; y < 0
 	JGE P10
 	mov eax, TRES_PI_CUARTOS
 	neg eax
-	mov theta, eax
+	;mov theta, eax
+
+	mov esi, offset arreglo
+	mov edx, count
+	mov [esi+edx], eax
+	;mov eax, [esi+edx]
+	add edx, 4
+	mov count, edx
+	;call writeint
 ;-----------------Sixth octant
 P10:
+	mov eax, abs_x
 	cmp eax, abs_y ; abs_x < abs_y
 	JGE P11
-	cmp ebx, 0 ; x < 0
+	cmp x, 0 ; x < 0
 	JGE P11
-	cmp edx, 0 ; y < 0
+	cmp y, 0 ; y < 0
 	JGE P11
 	mov eax, numerator
 	mov ebx, denominatorQ
@@ -252,66 +335,140 @@ P10:
 	neg edx
 	sub edx, eax
 	mov theta, edx
+	mov eax, edx
+
+	mov esi, offset arreglo
+	mov edx, count
+	mov [esi+edx], eax
+	;mov eax, [esi+edx]
+	add edx, 4
+	mov count, edx
+	;call writeint
 ;----------------- -90 Degrees	
 P11:
-	cmp ebx, 0 ; x == 0
+	mov eax, abs_x
+	cmp x, 0 ; x == 0
 	JNE P12
-	cmp edx, 0 ; y < 0
+	cmp y, 0 ; y < 0
 	JGE P12
 	mov eax, PI_MEDIOS
 	neg eax 
-	mov theta, eax
+	;mov theta, eax
+
+	mov esi, offset arreglo
+	mov edx, count
+	mov [esi+edx], eax
+	;mov eax, [esi+edx]
+	add edx, 4
+	mov count, edx
+	;call writeint
 ;-----------------Seventh octant
 P12:
+	mov eax, abs_x
 	cmp eax, abs_y ; abs_x < abs_y
 	JGE P13
-	cmp ebx, 0 ; x > 0
+	cmp x, 0 ; x > 0
 	JLE P13
-	cmp edx, 0 ; y < 0
+	cmp y, 0 ; y < 0
 	JGE P13
 	mov eax, numerator
+	neg eax
 	mov ebx, denominatorQ
 	xor edx, edx
 	idiv ebx
+	neg eax
 	mov edx, PI_MEDIOS 
 	neg edx
 	sub edx, eax
 	mov theta, edx
+	mov eax, edx
+
+	mov esi, offset arreglo
+	mov edx, count
+	mov [esi+edx], eax
+	;mov eax, [esi+edx]
+	add edx, 4
+	mov count, edx
+	;call writeint
 ;----------------- -45 Degrees	
 P13:
-	cmp eax, abs_y ; abx_x == abx_y
+	mov eax, abs_x
+	cmp eax, abs_y ; abs_x == abx_y
 	JNE P14
-	cmp ebx, 0 ; x > 0
+	cmp x, 0 ; x > 0
 	JLE P14
-	cmp edx, 0 ; y < 0
+	cmp y, 0 ; y < 0
 	JGE P14
 	mov eax, PI_CUARTOS
 	neg eax
-	mov theta, eax
+	;mov theta, eax
+
+	mov esi, offset arreglo
+	mov edx, count
+	mov [esi+edx], eax
+	;mov eax, [esi+edx]
+	add edx, 4
+	mov count, edx
+	;call writeint
 ;-----------------eighth octant
 P14:
+	mov eax, abs_x
 	cmp eax, abs_y ; abs_x > abx_y
 	JLE P15
-	cmp ebx, 0 ; x > 0
+	cmp x, 0 ; x > 0
 	JLE P15
-	cmp edx, 0 ; y < 0
+	cmp y, 0 ; y < 0
 	JGE P15
 	mov eax, numerator
+	neg eax
 	mov ebx, denominatorI
 	xor edx, edx
 	idiv ebx
-	mov theta, eax	
+	neg eax
+	;mov theta, eax	
+
+	mov esi, offset arreglo
+	mov edx, count
+	mov [esi+edx], eax
+	;mov eax, [esi+edx]
+	add edx, 4
+	mov count, edx
+	;call writeint
 ;-----------------180 Degrees
 P15:
-	cmp ebx, 0 ; x < 0
+	mov eax, abs_x
+	cmp x, 0 ; x < 0
 	JGE P16 
-	cmp edx, 0 ; y == 0
+	cmp y, 0 ; y == 0
 	JNE P16
 	mov eax, PI
-	mov theta, eax
+	;mov theta, eax
+
+	mov esi, offset arreglo
+	mov edx, count
+	mov [esi+edx], eax
+	;mov eax, [esi+edx]
+	add edx, 4
+	mov count, edx
+	;call writeint
 
 P16:
 	NOP
+
+cmp ecx, 1
+jne P17
+
+mov esi, offset arreglo
+INVOKE Write_Values
+
+pop esi
+xor ecx, ecx
+
+P17:
+	NOP
+
+
+
 
 ret
 ac_atan2 ENDP
